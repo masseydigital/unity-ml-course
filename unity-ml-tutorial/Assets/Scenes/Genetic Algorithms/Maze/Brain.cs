f14e00696087a154dfb9584074cac2b7ab47a271
masseydigital <acemgaming@gmail.com>
2019-10-16 23:32:32 -0400

    Adding maze learning.

---
+++ unity-ml-tutorial/Assets/Scenes/Genetic Algorithms/Maze/Brain.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Maze
+{
+    public class Brain : MonoBehaviour
+    {
+        public Dna dna;
+        public GameObject eyes;
+        public float distTravelled = 0;
+
+        private int dnaLength = 2;
+        private bool seeWall = true;
+        private Vector3 startPosition;
+        private bool alive = true;
+
+        #region Unity Methods
+        private void OnCollisionEnter(Collision collision)
+        {
+           if(collision.gameObject.tag == "dead")
+           {
+                distTravelled = 0;
+                alive = false;
+           }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!alive) return;
+
+            seeWall = false;
+            RaycastHit hit;
+            Debug.DrawRay(eyes.transform.position, eyes.transform.forward * 0.5f, Color.red);
+            if (Physics.SphereCast(eyes.transform.position, 0.1f, eyes.transform.forward, out hit, 0.5f))
+            {
+                if (hit.collider.gameObject.tag == "wall")
+                {
+                    seeWall = true;
+                }
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (!alive) return;
+
+            // read dna
+            float h = 0;
+            float v = dna.GetGene(0);
+
+            if (seeWall)
+            {
+                h = dna.GetGene(1);
+            }
+
+            this.transform.Translate(0, 0, v * 0.001f);
+            this.transform.Rotate(0, h, 0);
+            distTravelled = Vector3.Distance(startPosition, this.transform.position);
+        }
+        #endregion Unity Methods
+
+        #region Methods
+        // initialize dna
+        // 0 forward
+        // 1 angle turn
+        public void Init()
+        {
+            dna = new Dna(dnaLength, 360);
+            startPosition = this.transform.position;
+        }
+        #endregion Methods
+    }
+}
+
+
---
+++ unity-ml-tutorial/Assets/Scenes/Genetic Algorithms/Maze/PopulationManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace Maze
+{
+    public class PopulationManager : MonoBehaviour
+    {
+        public static float elapsed = 0;
+        public GameObject botPrefab;
+        public GameObject startPos;
+        public int populationSize = 50;
+        public float trialTime = 5;
+
+        private List<GameObject> population = new List<GameObject>();
+        private int generation = 1;
+
+        #region Unity Methods
+        // Start is called before the first frame update
+        void Start()
+        {
+            for(int i=0; i<populationSize; i++)
+            {
+                GameObject b = Instantiate(botPrefab, startPos.transform.position, this.transform.rotation);
+                b.GetComponent<Brain>().Init();
+                population.Add(b);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            elapsed += Time.deltaTime;
+            if(elapsed >= trialTime)
+            {
+                BreedPopulation();
+                elapsed = 0;
+            }
+        }
+
+        GUIStyle guiStyle = new GUIStyle();
+        private void OnGUI()
+        {
+            guiStyle.fontSize = 25;
+            guiStyle.normal.textColor = Color.white;
+            GUI.BeginGroup(new Rect(10, 10, 250, 150));
+            GUI.Box(new Rect(0, 0, 140, 140), "Stats", guiStyle);
+            GUI.Label(new Rect(10, 25, 200, 30), $"Gen: {generation}", guiStyle);
+            GUI.Label(new Rect(10, 50, 200, 30), string.Format("Time: {0:0:00}", elapsed), guiStyle);
+            GUI.Label(new Rect(10, 75, 200, 30), $"Population: {population.Count}", guiStyle);
+            GUI.EndGroup();
+        }
+        #endregion Unity Methods
+
+
+        #region Methods
+        private GameObject Breed(GameObject parent1, GameObject parent2)
+        {
+            GameObject offspring = Instantiate(botPrefab, startPos.transform.position, this.transform.rotation);
+            Brain b = offspring.GetComponent<Brain>();
+
+            if (Random.Range(0,100) == 1) //mutate 1%
+            {
+                b.Init();
+                b.dna.Mutate();
+            }
+            else
+            {
+                b.Init();
+                b.dna.Combine(parent1.GetComponent<Brain>().dna, parent2.GetComponent<Brain>().dna);
+            }
+
+            return offspring;
+        }
+
+        private void BreedPopulation()
+        {
+            List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<Brain>().distTravelled).ToList();
+
+            population.Clear();
+            for(int i = (int)(sortedList.Count/2.0f) - 1; i < sortedList.Count-1; i++)
+            {
+                for(int j = (int)(sortedList.Count/2.0f) + 1; i < sortedList.Count -1; i++)
+                {
+                    population.Add(Breed(sortedList[i], sortedList[j]));
+                    population.Add(Breed(sortedList[j], sortedList[i]));
+                }
+            }
+
+            //destroy all parents
+            for(int i=0; i<sortedList.Count; i++)
+            {
+                Destroy(sortedList[i]);
+            }
+
+            generation++;
+        }
+        #endregion Methods
+    }
+}
+
+
